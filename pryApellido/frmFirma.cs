92d9e8db62eb0e90565f4de023a20dd34a7a65e6
Laboratorios de Informática IES 21 <laboratoriosies@hotmail.com>
2024-03-27 12:45:32 -0300

    agrego un formulario

---
+++ pryApellido/frmFirma.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryApellido
+{
+    public partial class frmFirma : Form
+    {
+        public frmFirma()
+        {
+            InitializeComponent();
+        }
+
+        private void frmFirma_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "algo";
+        }
+    }
+}
